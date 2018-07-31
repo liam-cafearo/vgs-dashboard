a4a88c1073bd3dbb38973f5470dec2bb674fe3a3
Liam-Cafearo <lcafearo@gmail.com>
2018-07-31 21:57:09 +0100

    separated datatable script to avoid errors on main dashboard

---
+++ static/js/table.js
@@ -0,0 +1,38 @@
+var tabledData = dc.dataTable("#data-table");
+tabledData
+    // TODO figure out what dimension to place in here.
+    .dimension()
+    .columns([
+        function (d) {
+            return d.Name;
+        },
+        function (d) {
+            return d.Platform;
+        },
+        function (d) {
+            return d.Year;
+        },
+        function (d) {
+            return d.Genre;
+        },
+        function (d) {
+            return d.Publisher;
+        },
+        function (d) {
+            return d.NA_Sales;
+        },
+        function (d) {
+            return d.EU_Sales;
+        },
+        function (d) {
+            return d.JP_Sales;
+        },
+        function (d) {
+            return d.Other_Sales;
+        },
+        function (d) {
+            return d.Global_Sales;
+        }
+    ]);
+
+dc.renderAll();
--- static/js/visualization.js
+++ static/js/visualization.js
@@ -184,41 +184,5 @@ function createGraphs(error, videoGameSales) {
         .dimension(platformDim)
         .group(numVideoGameSalesByPlatform);
 
-    var tabledData = dc.dataTable("#data-table");
-    tabledData
-        .group(all)
-        .columns([
-            function (d) {
-                return d.Name;
-            },
-            function (d) {
-                return d.Platform;
-            },
-            function (d) {
-                return d.Year;
-            },
-            function (d) {
-                return d.Genre;
-            },
-            function (d) {
-                return d.Publisher;
-            },
-            function (d) {
-                return d.NA_Sales;
-            },
-            function (d) {
-                return d.EU_Sales;
-            },
-            function (d) {
-                return d.JP_Sales;
-            },
-            function (d) {
-                return d.Other_Sales;
-            },
-            function (d) {
-                return d.Global_Sales;
-            }
-        ]);
-
     dc.renderAll();
 };
